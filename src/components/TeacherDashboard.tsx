4fc41f97385a17b307d10b2ba2b64efee2dddc81
Burak <55651089+BurakGuven1@users.noreply.github.com>
2025-09-29 18:14:21 +0300

    Updated TeacherDashboard.tsx

--- src/components/TeacherDashboard.tsx
+++ src/components/TeacherDashboard.tsx
@@ -379,6 +379,7 @@ export default function TeacherDashboard() {
                       </button>
                     </div>
                   </div>
+                </div>
                   );
                 })()
               ))}
